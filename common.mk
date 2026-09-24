f1aa9a2e4b919a7daaa03acc17dfcdc94a1cc122
y3wad <ynsa310@gmail.com>
2026-09-24 15:17:57 +0300

    sm6225-common: remove Dolby Atmos audio effects

--- common.mk
+++ common.mk
@@ -151,8 +151,6 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     libsuspend
 
-# Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
